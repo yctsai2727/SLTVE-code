e5d669fcb7beaa31d0f23073016e11b2f263de2f
yctsai2727 <yctsai@connect.ust.hk>
2024-02-05 17:56:33 +0800

    improved memory usage

--- driveCN.m
+++ driveCN.m
@@ -30,7 +30,7 @@ id_x=mod(id,4);
 id_y=floor(id/4);
 
 %rank approximation
-r=15;
+r=25;
 decoder = @(M) LowRankDecoder(M,r,m,n);
 
 %initialization
@@ -41,59 +41,39 @@ dtK = (finalt-dt0)/(K-1);
 miu = D0 * dt / dx^2;
 M1 = left_mat(miu, m, n);
 M2 = right_mat(miu, m, n);
-traj = zeros(m_p,n_p,K,r,m+n+1);
-cache = zeros(m+1,n+1,2); #1->up, 2->left
-flag = zeros(m,n);
+traj = zeros(n_p+2,K,r,m+n+1);
+cache = zeros(n_p+1,1); #1 - n_p: left-cache, n_p+1: up-cache
 sol = zeros(m_p,n_p);
 
 %main loop
-for i = 1:m_p
-    for j = 1:n_p
+for i = 0:m_p+1
+    for j = 0:n_p+1
         [i, j]
-        if id_x*m_p+i>m || id_y*n_p+j>n
-            break
+        if id_x*m_p+i>0 && id_x*m_p+i<=m && id_y*n_p+j>0 && id_y*n_p+j<=n
+            x0 = x(id_x*m_p+i, 1);
+            y0 = y(1, id_y*n_p+j);
+            t = dt0;
+            pdf = 1 / (4 * pi * D0 * dt0) * exp(-1 / (4 * D0 * dt0) * ((x - x0).^2 + (y - y0).^2));
+            curr = Solver(x,y,x0,y0,t,finalt,dx,dy,dt,dtK,pdf,m,n,K,r,M1,M2);
+        else
+            curr = zeros(K,r,m+n+1);
         end
-        x0 = x(id_x*m_p+i, 1);
-        y0 = y(1, id_y*n_p+j);
-        t = dt0;
         down=0;
         right=0;
-        if flag(i,j)==0
-            pdf = 1 / (4 * pi * D0 * dt0) * exp(-1 / (4 * D0 * dt0) * ((x - x0).^2 + (y - y0).^2));
-            traj(i,j,:,:,:)=Solver(x,y,x0,y0,t,finalt,dx,dy,dt,dtK,pdf,m,n,K,r,M1,M2);
-            %size(traj(i,j,:,:,:))
-            flag(i,j)=1;
+        if j==0 && i>0
+            cache(n_p+1) = MatTrajMetric(traj(j+1,:,:,:),traj(j+2,:,:,:),method(2),decoder);
+        elseif j<n_p+1
+            if i==1
+                cache(j) = MatTrajMetric(traj(j+1,:,:,:),curr,method(1),decoder);
+            elseif i>0
+                right = MatTrajMetric(traj(j+1,:,:,:),curr,method(1),decoder);
+                down = MatTrajMetric(traj(j+1,:,:,:),traj(j+2,:,:,:),method(2),decoder);
+                sol(i-1,j) = max(max(cache(n_p+1),down),max(cache(j),right));
+                cache(j) = right;
+                cache(n_p+1) = down;
+            end
         end
-        if i>1 && flag(i-1,j)==0    #left
-            Lpdf = (1 / (4 * pi * D0 * dt0) * exp(-1 / (4 * D0 * dt0) * ((x - x0+dx).^2 + (y - y0).^2)));
-            traj(i-1,j,:,:,:)=Solver(x,y,x0-dx,y0,t,finalt,dx,dy,dt,dtK,Lpdf,m,n,K,r,M1,M2);
-            flag(i-1,j)=1;
-            cache(i,j,2)=MatTrajMetric(traj(i-1,j,:,:,:),traj(i,j,:,:,:),method(1),decoder);
-        end
-        if j>1 && flag(i,j-1)==0    #up
-            Updf = (1 / (4 * pi * D0 * dt0) * exp(-1 / (4 * D0 * dt0) * ((x - x0).^2 + (y - y0+dy).^2)));
-            traj(i,j-1,:,:,:)=Solver(x,y,x0,y0-dy,t,finalt,dx,dy,dt,dtK,Updf,m,n,K,r,M1,M2);
-            flag(i,j-1)=1;
-            cache(i,j,1)=MatTrajMetric(traj(i,j-1,:,:,:),traj(i,j,:,:,:),method(2),decoder);
-        end
-        if i<m  #right
-            Rpdf = (1 / (4 * pi * D0 * dt0) * exp(-1 / (4 * D0 * dt0) * ((x - x0-dx).^2 + (y - y0).^2)));
-            traj(i+1,j,:,:,:)=Solver(x,y,x0+dx,y0,t,finalt,dx,dy,dt,dtK,Rpdf,m,n,K,r,M1,M2);
-            flag(i+1,j)=1;
-            right=MatTrajMetric(traj(i,j,:,:,:),traj(i+1,j,:,:,:),method(1),decoder);
-        end
-        if j<n  #down
-            Dpdf = (1 / (4 * pi * D0 * dt0) * exp(-1 / (4 * D0 * dt0) * ((x - x0).^2 + (y - y0-dy).^2)));
-            traj(i,j+1,:,:,:)=Solver(x,y,x0,y0+dy,t,finalt,dx,dy,dt,dtK,Dpdf,m,n,K,r,M1,M2);
-            flag(i,j+1)=1;
-            down=MatTrajMetric(traj(i,j,:,:,:),traj(i,j+1,:,:,:),method(2),decoder);
-        end
-        sol(i,j)=max(max(cache(i,j,1),down),max(cache(i,j,2),right));
-        cache(i+1,j,2)=right;
-        cache(i,j+1,1)=down;
-    end
-    if id_x*m_p+i>m
-        break
+        traj(j+1,:,:,:) = curr;
     end
 end
 
